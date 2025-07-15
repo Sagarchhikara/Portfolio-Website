b174d0e2be55db97678fcbb0cdb8a1209413920e
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-15 14:35:25 +0530

    Update content in main app and components
    Modified src/App.js, About.js, Home.js, and ProjectBox.js to update displayed content and improve structure. These changes enhance the user experience and keep the information current.

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,30 +11,30 @@ const About = () => {
         <div className='AboutText'>
           <h1 className='AboutTextHeading' >Get to <b>know</b> me!</h1>
           <p>
-            Hi, my name is <b>Devansh Sahni</b> and I am from New Delhi, India.
-            I'm a <b>MERN stack web developer</b> and a final year college student pursuing <b>BTech in CSE</b>. <br/><br/>
-            I have done an internship as a <b>software developer</b> at GoalFi which is a fintech startup.
-            I love to create original projects with beautiful designs, you can check out some of my work in the projects section.<br/><br/>
-            I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br/>
-            Apart from coding I love to do photography, you can check out some of my shots here <a href="https://www.instagram.com/devansh_sahni_/?hl=en" target='_blank'>Instagram.</a>
+            Hi, my name is <b>Sagar Chhikara </b> and I am from Chandigarh, India.
+            I'm a <b>MERN stack web developer</b> and a Second year college student pursuing <b>BTech in CSE</b>. <br /><br />
+
+            I love to create original projects with beautiful designs, you can check out some of my work in the projects section.<br /><br />
+            I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br />
+            Apart from coding I love to do photography, you can check out some of my shots here <a href="https://www.instagram.com/Sagar_.chhikara/?hl=en" target='_blank'>Instagram.</a>
           </p>
         </div>
 
         <div>
           <Tilt>
-              <Lottie 
-              className="illustration" 
-              animationData={Coder} 
-              loop={true} 
+            <Lottie
+              className="illustration"
+              animationData={Coder}
+              loop={true}
             />
           </Tilt>
         </div>
 
       </div>
-      
+
       <h1 className='SkillsHeading'>Professional Skillset</h1>
       <div className='skills'>
-        
+
         <Skills skill='React' />
         <Skills skill='Node' />
         <Skills skill='Express' />
@@ -47,8 +47,8 @@ const About = () => {
         <Skills skill='Figma' />
         <Skills skill='Vercel' />
         <Skills skill='Npm' />
-        <Skills skill='Bootstrap'/>
-        
+        <Skills skill='Bootstrap' />
+
       </div>
     </>
   )
